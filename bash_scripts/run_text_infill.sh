373b945af090078b045168446b3ba6a12b6f6256
xe321was <ppynadat@purdue.edu>
2025-08-06 17:48:16 -0700

    updated the bash script for running text infill

--- bash_scripts/run_text_infill.sh
+++ bash_scripts/run_text_infill.sh
@@ -1,2 +1,4 @@
-python run_text_sampling.py --num_cycles 5 --samples_per_example 25 --n_steps 100 --inference_mask_p .35 --examples 100 --dataset_name grimm
-python run_text_sampling.py --num_cycles 5 --samples_per_example 25 --n_steps 100 --inference_mask_p .35 --examples 100 --dataset_name sst2
+#!/bin/bash
+
+python run_text_sampling.py --num_cycles 5 --samples_per_example 25 --n_steps 100 --inference_mask_p .5 --examples 10 --dataset_name grimm
+python run_text_sampling.py --num_cycles 5 --samples_per_example 25 --n_steps 100 --inference_mask_p .5 --examples 10 --dataset_name sst2
